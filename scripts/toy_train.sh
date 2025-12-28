30d9d28c12ce401fbcf58ad923dff1fa194167d4
kuailehaha <kuailepapa@gmail.com>
2025-12-29 00:03:34 +0800

    修改toy train

--- scripts/toy_train.sh
+++ scripts/toy_train.sh
@@ -3,17 +3,18 @@ export PYTHONPATH=/home/ubuntu/.cache/liuzhou:/home/ubuntu/.cache/liuzhou/v0/bui
 
 # 先测试自博弈
 python -m v0.generate_data \
-  --num_games 4 \
-  --mcts_simulations 50 \
+  --num_games 64 \
+  --mcts_simulations 800 \
   --batch_leaves 256 \
   --device cuda
 
 # 再测试训练
 python -m v0.train \
+
   --data_files ./v0/data/self_play/*.jsonl \
-  --iterations 1 \
-  --epochs 2 \
-  --batch_size 16 \
+  --iterations 3 \
+  --epochs 5 \
+  --batch_size 128 \
   --device cuda \
-  --eval_games_vs_random 4 \
+  --eval_games_vs_random 100 \
   --eval_games_vs_best 0
