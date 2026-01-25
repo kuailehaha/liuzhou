5c1cc99b349741c5edf951e6ccface5eded17253
kuailehaha <kuailepapa@gmail.com>
2026-01-25 21:17:52 +0800

    更新训练参数

--- scripts/toy_train.sh
+++ scripts/toy_train.sh
@@ -22,9 +22,9 @@ export PYTHONPATH="/home/ubuntu/.cache/liuzhou:/home/ubuntu/.cache/liuzhou/v0/bu
 # v0 版训练脚本
 python -m v0.train \
   --iterations 40 \
-  --self_play_workers 8 \
+  --self_play_workers 12 \
   --self_play_games_per_worker 80 \
-  --mcts_simulations 512 \
+  --mcts_simulations 1024 \
   --self_play_batch_leaves 512 \
   --self_play_inference_batch_size 512 \
   --self_play_opening_random_moves 8 \
@@ -34,10 +34,11 @@ python -m v0.train \
   --epochs 5 \
   --value_draw_weight 0.1 \
   --policy_draw_weight 0.3 \
-  --batch_size 256 \
+  --batch_size 2048 \
   --device cuda \
   --eval_games_vs_random 200 \
   --eval_games_vs_best 200 \
   --mcts_sims_eval 256 \
   --eval_backend v0 \
-  --checkpoint_dir ./checkpoints_v0
+  --checkpoint_dir ./checkpoints_v0 \
+  --load_checkpoint ./checkpoints_v0/best_model.pt
